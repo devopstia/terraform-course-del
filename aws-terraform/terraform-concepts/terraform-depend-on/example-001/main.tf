77a9d140c1809a870612af0907cf0c9ed282c582
Merluise Tia <Tia Leo>
2023-12-21 07:28:39 -0600

    terraform

---
+++ aws-terraform/terraform-concepts/terraform-depend-on/example-001/main.tf
@@ -0,0 +1,65 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Create VPC
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+}
+
+# Create public subnet within the VPC
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+}
+
+# Create security group allowing inbound traffic on port 22 (SSH)
+resource "aws_security_group" "allow_ssh" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Launch an EC2 instance in the public subnet
+resource "aws_instance" "example" {
+  ami                    = "ami-0fc5d935ebf8bc3bc"
+  instance_type          = "t2.micro"
+  key_name               = "terraform-aws"
+  subnet_id              = aws_subnet.public_subnet.id
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+
+  root_block_device {
+    volume_size = "10"
+  }
+
+  tags = {
+    Name      = "vm"
+    Create_By = "Terraform"
+  }
+
+  depends_on = [
+    aws_subnet.public_subnet,
+    aws_security_group.allow_ssh,
+  ]
+}
+
+
---
+++ aws-terraform/terraform-concepts/terraform-user-data/example-001/main.tf
@@ -0,0 +1,53 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami                    = "ami-0fc5d935ebf8bc3bc"
+  instance_type          = "t2.micro"
+  key_name               = "terraform-aws"
+  vpc_security_group_ids = ["sg-0c51540c60857b7ed"]
+  subnet_id              = "subnet-096d45c28d9fb4c14"
+  root_block_device {
+    volume_size = "10"
+  }
+
+  tags = {
+    "Name"           = "bastion-host"
+    "id"             = "2560"
+    "owner"          = "DevOps Easy Learning an Training"
+    "teams"          = "DEL"
+    "environment"    = "dev"
+    "project"        = "del"
+    "create_by"      = "Terraform"
+    "cloud_provider" = "aws"
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              apt-get update
+              apt-get install -y apache2
+              
+              cat <<EOL > /var/www/html/index.html
+              <!DOCTYPE html>
+              <html>
+              <body>
+              <h1>Welcome to DevOps Easy Learning</h1>
+              </body>
+              </html>
+              EOL
+              
+              systemctl enable apache2
+              systemctl start apache2
+              EOF
+}
