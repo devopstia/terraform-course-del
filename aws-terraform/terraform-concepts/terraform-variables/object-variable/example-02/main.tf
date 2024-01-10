86f731a685b985f6fab01e36e8c275a4185e3aba
Tia Leo <info@devopseasylearning>
2024-01-10 14:18:42 -0600

    DevOps Easy Learning

--- aws-terraform/terraform-concepts/terraform-variables/object-variable/example-02/main.tf
+++ aws-terraform/terraform-concepts/terraform-variables/object-variable/example-02/main.tf
@@ -29,10 +29,10 @@ variable "instance_configs" {
     vpc_security_group_ids = ["sg-0c51540c60857b7ed"]
     subnet_id              = "subnet-096d45c28d9fb4c14"
     volume_size            = "10"
-    tags = {
-      Name      = "vm-1"
-      Create_By = "Terraform"
-    }
+    # tags = {
+    #   Name      = "vm-1"
+    #   Create_By = "Terraform"
+    # }
   }
 }
 
@@ -48,7 +48,7 @@ resource "aws_instance" "example" {
     volume_size = try(var.instance_configs.volume_size, "10")
   }
   tags = try(var.instance_configs.tags, {
-    Name      = "vm-1"
+    Name      = "bastion-host"
     Create_By = "Terraform"
   })
 }
